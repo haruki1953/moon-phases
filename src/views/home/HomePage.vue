6aa25b320ac4dc25b104d54a49b57e19d415b108
haruki <kinikun@outlook.com>
2025-12-28 14:13:43 +0800

    feat: Implement responsive layout

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -14,6 +14,11 @@ import { useMoonRotation, useZoomControl } from './composables'
 const layoutBigXDistanceNeedFree = 100 as const
 // 上下需空出的距离
 const layoutBigYDistanceNeedFree = 20 as const
+// 窄屏
+// 左右需空出的距离
+const layoutSmallXDistanceNeedFree = 20 as const
+// 上下需空出的距离
+const layoutSmallYDistanceNeedFree = 20 as const
 
 const windowSize = useWindowSize()
 
@@ -25,33 +30,40 @@ const { controlZoomVal } = zoomControl
 const moonRotation = useMoonRotation()
 
 // 内容布局
-// width 负责保证左右空出
-// maxWidth 负责保证上下空出
-const contentWidthOnLayoutBig = computed(() => {
-  const layoutBigXDistanceNeedFreeX2 = layoutBigXDistanceNeedFree * 2
-  const widthNum = windowSize.width.value - layoutBigXDistanceNeedFreeX2
-  if (widthNum <= 0) {
-    return undefined
-  }
-  const widthNumWithZoom = widthNum * controlZoomVal.value
-  return `${widthNumWithZoom}px` as const
-  // return 'calc(100% - 200px)' as const
+const isLayoutBig = computed(() => {
+  const ratio = windowSize.width.value / windowSize.height.value
+  return ratio > 1.3 && windowSize.width.value > 600
 })
-const contentMaxWidthOnLayoutBig = computed(() => {
-  const layoutBigYDistanceNeedFreeX2 = layoutBigYDistanceNeedFree * 2
-  const maxWidthNum = windowSize.height.value - layoutBigYDistanceNeedFreeX2
-  if (maxWidthNum <= 0) {
-    return undefined
-  }
-  const maxWidthNumWithZoom = maxWidthNum * controlZoomVal.value
-  return `${maxWidthNumWithZoom}px` as const
+
+const contentSizeOnLayoutBig = computed(() => {
+  const usableWidth = windowSize.width.value - layoutBigXDistanceNeedFree * 2
+  const usableHeight = windowSize.height.value - layoutBigYDistanceNeedFree * 2
+
+  const baseSize = Math.min(usableWidth, usableHeight)
+  if (baseSize <= 0) return undefined
+
+  return `${baseSize * controlZoomVal.value}px` as const
+})
+
+const contentSizeOnLayoutSmall = computed(() => {
+  const usableWidth = windowSize.width.value - layoutSmallXDistanceNeedFree * 2
+  const usableHeight =
+    windowSize.height.value - layoutSmallYDistanceNeedFree * 2
+
+  const baseSize = Math.min(usableWidth, usableHeight)
+  if (baseSize <= 0) return undefined
+
+  return `${baseSize * controlZoomVal.value}px` as const
 })
 </script>
 
 <template>
   <div>
     <!-- 宽屏 中间为内容 两侧为按钮 -->
-    <div>
+    <!-- 响应式TODO -->
+    <!-- 屏幕宽除以高大于某值可配置 且 屏幕宽度需大于 600 -->
+    <div v-if="isLayoutBig">
+      <!-- w-max 目的就是在放大时让内容撑开，以显示横向滚动条 -->
       <div
         class="relative flex min-h-screen w-max min-w-full items-center justify-center"
       >
@@ -65,10 +77,10 @@ const contentMaxWidthOnLayoutBig = computed(() => {
         <div
           class="my-4 shrink-0 transition-all duration-500"
           :style="{
-            width: contentWidthOnLayoutBig,
-            maxWidth: contentMaxWidthOnLayoutBig,
+            width: contentSizeOnLayoutBig,
           }"
         >
+          <!-- MoonPhasesCalendarWheel 中有比例显示，所以不需要显式得设置高度，根据计算指定maxWidth来控制高度已实现 -->
           <MoonPhasesCalendarWheel
             :moonRotation="moonRotation"
           ></MoonPhasesCalendarWheel>
@@ -89,8 +101,75 @@ const contentMaxWidthOnLayoutBig = computed(() => {
         </div>
       </div>
     </div>
-    <!-- 窄屏 上半部分为内容 下半部分为按钮 整体居中 TODO -->
-    <!-- <div></div> -->
+    <!-- 窄屏 内容 按钮 需整体居中 TODO -->
+    <!-- 需根据缩放值缩放（1到3倍） -->
+    <!-- 内容默认宽度为屏幕宽度（有一些边距） -->
+    <!-- 按钮在内容下方，为三列 -->
+    <!-- 左列 -->
+    <!-- 中列 MoonButtonGroupZoom MoonButtonGroupDarkProject -->
+    <!-- 右列 -->
+    <div v-else>
+      <div class="relative w-max min-w-full">
+        <div class="flex min-h-screen items-center justify-center">
+          <div
+            class="transition-all duration-500"
+            :style="{
+              width: contentSizeOnLayoutSmall,
+            }"
+          >
+            <!-- 内容 -->
+            <div class="">
+              <div class="mx-4 mb-10 mt-4">
+                <MoonPhasesCalendarWheel :moonRotation="moonRotation" />
+              </div>
+            </div>
+            <!-- 按钮组 -->
+            <div v-if="false">
+              <div class="flex items-center justify-center">
+                <!-- 左列 -->
+                <div>
+                  <MoonButtonGroupLeft :moonRotation="moonRotation" />
+                </div>
+                <!-- 中列 MoonButtonGroupZoom MoonButtonGroupDarkProject -->
+                <div>
+                  <MoonButtonGroupZoom :zoomControl="zoomControl" />
+                </div>
+                <div>
+                  <MoonButtonGroupDarkProject />
+                </div>
+                <!-- 右列 -->
+                <div>
+                  <MoonButtonGroupRight :moonRotation="moonRotation" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 按钮组 -->
+        <div class="absolute bottom-0 left-0 right-0">
+          <div class="flex items-end justify-between">
+            <!-- 左 -->
+            <div class="flex items-end">
+              <div>
+                <MoonButtonGroupLeft :moonRotation="moonRotation" />
+              </div>
+              <div>
+                <MoonButtonGroupZoom :zoomControl="zoomControl" />
+              </div>
+            </div>
+            <!-- 右 -->
+            <div class="flex items-end">
+              <div>
+                <MoonButtonGroupDarkProject />
+              </div>
+              <div>
+                <MoonButtonGroupRight :moonRotation="moonRotation" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
